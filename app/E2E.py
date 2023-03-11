dd32819e0fc470f7425d408bd89d302a21d4c902
jack B.M <yosef3141@gmail.com>
2023-03-11 21:06:03 +0200

    E2E test improvement

--- app/E2E.py
+++ app/E2E.py
@@ -1,22 +1,46 @@
-import requests,random
+import requests
+import random
+import unittest
 
+# Define constants for URLs
+BASE_URL = 'http://localhost'
+FILE_LIST_URLS = [
+    '/file_list/Black_Lagoon',
+    '/file_list/ChainsawMan',
+    '/file_list/CowboyBebop',
+    '/file_list/BakiHanma',
+    '/file_list/Dr.Stone',
+    '/file_list/AOT'
+]
 
-def test_local():
-    response = requests.get('http://localhost/')
-    assert response.status_code == 200
+class CatalogTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.headers = {'Accept': 'application/json'}
 
-def test_file_list():
-    response = requests.get('http://localhost/file_list/AOT')
-    assert response.status_code == 200
+    def test_get_localhost(self):
+        url = f"{BASE_URL}"
+        res = requests.get(url)
+        self.assertEqual(res.status_code, 200)
 
-    for i in range(5):
-        episode_num = str(random.randint(1, 10)).zfill(2)
-        episode_name = f'Attack_on_Titan_S01E{episode_num}.mp4'
-        episode_name = bytes(episode_name, 'utf-8')
-        assert episode_name in response.content
+    def test_get_404(self):
+        url = f"{BASE_URL}/nonexistent"
+        res = requests.get(url)
+        self.assertEqual(res.status_code, 404)
 
+    def test_get_file_list(self):
+        for url in FILE_LIST_URLS:
+            res = requests.get(f"{BASE_URL}{url}")
+            self.assertEqual(res.status_code, 200)
 
-def error():
-    response = requests.get('http://localhost/homer_simpson')
-    assert response.status_code == 404
+    def test_get_episodes(self):
+        url = f"{BASE_URL}/file_list/Black_Lagoon"
+        res = requests.get(url, headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+        for i in range(5):
+            episode_number = str(random.randint(1, 10)).zfill(2)
+            episode_name = f"Black_Lagoon_S01E{episode_number}"
+            episode_name_bytes = bytes(episode_name, 'utf-8')
+            self.assertIn(episode_name_bytes, res.content)
+
+            
