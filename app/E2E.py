2a22416ecde4eabf9ef0857b267552d6a2c6cc50
jack B.M <yosef3141@gmail.com>
2023-03-12 11:25:56 +0200

    trig ci

--- app/E2E.py
+++ app/E2E.py
@@ -19,26 +19,28 @@ class CatalogTestCase(unittest.TestCase):
         self.headers = {'Accept': 'application/json'}
 
     def test_get_localhost(self):
-        url = "{}".format(BASE_URL)
+        url = f"{BASE_URL}"
         res = requests.get(url)
         self.assertEqual(res.status_code, 200)
 
     def test_get_404(self):
-        url = "{}/nonexistent".format(BASE_URL)
+        url = f"{BASE_URL}/nonexistent"
         res = requests.get(url)
         self.assertEqual(res.status_code, 404)
 
     def test_get_file_list(self):
         for url in FILE_LIST_URLS:
-            res = requests.get("{}{}".format(BASE_URL, url))
+            res = requests.get(f"{BASE_URL}{url}")
             self.assertEqual(res.status_code, 200)
 
     def test_get_episodes(self):
-        url = "{}/file_list/Black_Lagoon".format(BASE_URL)
+        url = f"{BASE_URL}/file_list/Black_Lagoon"
         res = requests.get(url, headers=self.headers)
         self.assertEqual(res.status_code, 200)
         for i in range(5):
             episode_number = str(random.randint(1, 10)).zfill(2)
-            episode_name = "Black_Lagoon_S01E{}".format(episode_number)
+            episode_name = f"Black_Lagoon_S01E{episode_number}"
             episode_name_bytes = bytes(episode_name, 'utf-8')
             self.assertIn(episode_name_bytes, res.content)
+
+            
